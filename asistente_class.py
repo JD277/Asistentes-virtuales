ef55d984346a5f43d139724f61ba74b52ba40cad
Ely1su5 <jesusitoduran23@gmail.com>
2025-01-28 19:28:55 -0400

    Feat(transcript): Se añade la caracteristica de speech to text

--- asistente_class.py
+++ asistente_class.py
@@ -55,3 +55,78 @@ class Asistente:
         except Exception as e:
             print("No se pudo reproducir el audio")
         os.remove(self.audio_path)
+    
+    def transcript(self):
+        try:
+            model = Model(self.vosk_path)
+        except Exception as e:
+            try:
+                model = Model(self.vosk_model_lang)
+            except Exception as e:
+                print("Error al cargar el modelo de voz")
+
+        q = queue.Queue()
+        def callback(indata, frames, time, status):
+            q.put(bytes(indata))
+        
+        silence_duration = 1.5
+        with sounddevice.RawInputStream(samplerate=16000, blocksize=8000, dtype="int16", channels=1, callback=callback):
+            print("Escuchando...")
+            silence_counter = 0
+            recognizer = KaldiRecognizer(model, 16000)
+            final_text = ""
+            while True:
+                data = q.get()
+
+                if recognizer.AcceptWaveform(data):
+                    result = json.loads(recognizer.Result())['text']
+                    silence_counter = 0
+                    final_text += f" {result}"
+                else:
+                    partial = json.loads(recognizer.PartialResult())['partial']
+                    print(partial)
+                    print(partial)
+                    if partial == "":
+                        silence_counter += 1
+                    print(silence_counter)
+
+                if silence_counter >= silence_duration * (16000 / 8000):
+                    print("Silence detected, stopping...")
+                    break
+            if final_text != "":
+                print("Listo!")
+                return final_text
+            else:
+                print("Listo!")
+                return ""
+
+gemini = Asistente("gemini-1.5-flash", "audio.mp3", "./models/vosk-model-small-es-0.42", "hola", config={"temperature":0.8,"top_p":0.95, "top_k":64, "max_output_tokens":8192, "response_mime_type": "text/plain"}, system_instruction="", API="AIzaSyDY1Ldl5_yOGcLzwbcj5gqe-LUNm4J--c0", vosk_model_lang="es")
+gemini.transcript()
+    def key_word(self):
+        try:
+            model = Model(self.vosk_path)
+        except Exception as e:
+            try:
+                model = Model(self.vosk_model_lang)
+            except Exception as e:
+                print("Error al cargar el modelo de voz")
+
+        q = queue.Queue()
+        def callback(indata, frames, time, status):
+            q.put(bytes(indata))
+        
+        with sounddevice.RawInputStream(samplerate=16000, blocksize=8000, dtype="int16", channels=1, callback=callback):
+            print("Escuchando...")
+            recognizer = KaldiRecognizer(model, 16000)
+            while True:
+                data = q.get()
+
+                if recognizer.AcceptWaveform(data):
+                    result = json.loads(recognizer.Result())['text']
+                    silence_counter = 0
+                    final_text += f" {result}"
+                else:
+                    partial = json.loads(recognizer.PartialResult())['partial']
+                    print(partial)
+                    print(partial)
+                    
